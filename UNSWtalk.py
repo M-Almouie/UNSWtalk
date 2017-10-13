4b037dc35c3121126fc219444711ad7d82dddf82
Mohamed Al Mouiee <m.almouiee@student.unsw.edu.au>
2017-10-13 15:40:53 +1100

    Implemented registration partially, still need email confirmation, login confirmation

--- UNSWtalk.py
+++ UNSWtalk.py
@@ -4,7 +4,8 @@
 # https://cgi.cse.unsw.edu.au/~cs2041/assignments/UNSWtalk/
 
 import os
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request
+import re
 
 students_dir = "dataset-small";
 
@@ -27,12 +28,42 @@ def start():
     with open(details_filename) as f:
         details = f.read()
     session['n'] = n + 1
-    return render_template('mainPage.html', student_details=details)
+    return render_template('mainPage.html',username=username,password=password)
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html')
+    error = None
+    return render_template('register.html',error = error)
+
+@app.route('/authenRegi',methods=['GET','POST'])
+def authenRegi():
+    username = request.form.get('username','')
+    #get Username from input
+    #fix up input conditions for security
+    password = request.form.get('password','')
+    if len(username) > 32:
+        username = username[:32]
+    username = re.sub(r'\@|\||\<|\>',"",username)
+    if len(password) > 32:
+        password = password[:32]
+    password = re.sub(r'\@|\||\<|\>',"",password)
+    email = request.form.get('email','')
+    email = request.form.get('email','')
+    if not re.match(r'\@',email):
+        error = "Invalid Email. Please enter a valid Email"
+        return render_template('register.html',error=error)
+    return render_template('mainPage.html',username=username,password=password)
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     app.run(debug=True)
+
+def displayFriends():
+    n = session.get('n', 0)
+    students = sorted(os.listdir(students_dir))
+    student_to_show = students[n % 3]
+    details_filename = os.path.join(students_dir,student_to_show, "student.txt")
+    with open(details_filename) as f:
+        details = f.read()
+    session['n'] = n + 1
+    return render_template('feed.html',student_details=details)
